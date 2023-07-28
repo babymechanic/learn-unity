4aed836d3da40849ce0c11bda097dcd41c823fe1
piggy <babymechanic@gmail.com>
2023-07-28 21:34:44 +1000

    prevent double effect applied

--- SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,10 +6,12 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] private float reloadDelay = 2.0f;
     [SerializeField] private ParticleSystem crashEffect;
     [SerializeField] private AudioClip crashSFX;
+    private bool crashed;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag("Ground")) return;
+        if (!other.CompareTag("Ground") || crashed) return;
+        crashed = true;
         crashEffect.Play();
         FindObjectOfType<PlayerController>().DisableControls();
         GetComponent<AudioSource>().PlayOneShot(crashSFX);
